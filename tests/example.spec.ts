5d4141c3fd8a69840e726bc3af27086a418ea75b
Edmund Hung <me@edmund.dev>
2022-07-01 23:57:00 +0200

    chore: read validation message from element to avoid inconsistency on different environment

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,72 +1,82 @@
 import { test, expect } from '@playwright/test';
+import type { Page } from '@playwright/test';
+
+async function getValidationMessage(page: Page, name: string): Promise<string> {
+	return page
+		.locator(`[name="${name}"]`)
+		.evaluate<
+			string,
+			HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+		>((field) => field.validationMessage);
+}
 
 test.describe('Search', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/search');
 	});
 
-	test('should work', async ({ page, browserName }) => {
-		const url = page.url();
+	test('should work', async ({ page }) => {
+		let url = page.url();
 
 		await page.locator('button[type="submit"]').click();
 
 		expect(page.url(), 'Submission should be blocked').toBe(url);
 
-		const error = {
-			keywordValueMissing: '',
-			keywordTooShort: '',
-			categoryValueMissing: '',
-		};
+		let keywordError: string;
+		let categoryError: string;
 
-		switch (browserName) {
-			case 'chromium':
-				error.keywordValueMissing = 'Please fill in this field.';
-				error.keywordTooShort =
-					'Please lengthen this text to 3 characters or more (you are currently using 2 characters).';
-				error.categoryValueMissing = 'Please select an item in the list.';
-				break;
-			case 'firefox':
-				error.keywordValueMissing = 'Please fill out this field.';
-				error.keywordTooShort =
-					'Please use at least 3 characters (you are currently using 2 characters).';
-				error.categoryValueMissing = 'Please select an item in the list.';
-				break;
-			case 'webkit':
-				error.keywordValueMissing = 'Fill out this field';
-				error.keywordTooShort = 'Use at least 3 characters';
-				error.categoryValueMissing = 'Select an item in the list';
-				break;
-		}
+		[keywordError, categoryError] = await Promise.all([
+			getValidationMessage(page, 'keyword'),
+			getValidationMessage(page, 'category'),
+		]);
 
-		await expect(
-			page.locator('p'),
-			'Both fields should be reported as value missing',
-		).toHaveText([error.keywordValueMissing, error.categoryValueMissing], {
+		await expect(page.locator('p')).toHaveText([keywordError, categoryError], {
 			useInnerText: true,
 		});
+
+		expect(keywordError).not.toBe('');
+		expect(categoryError).not.toBe('');
 
 		await page.locator('input[name="keyword"]').type('co');
-		await expect(
-			page.locator('p'),
-			'Keyword should be reported as too short',
-		).toHaveText([error.keywordTooShort, error.categoryValueMissing], {
+
+		[keywordError, categoryError] = await Promise.all([
+			getValidationMessage(page, 'keyword'),
+			getValidationMessage(page, 'category'),
+		]);
+
+		await expect(page.locator('p')).toHaveText([keywordError, categoryError], {
 			useInnerText: true,
 		});
 
+		expect(keywordError).not.toBe('');
+		expect(categoryError).not.toBe('');
+
 		await page.locator('input[name="keyword"]').type('conform');
-		await expect(page.locator('p'), 'Keyword should now be valid').toHaveText(
-			['', error.categoryValueMissing],
-			{
-				useInnerText: true,
-			},
-		);
+
+		[keywordError, categoryError] = await Promise.all([
+			getValidationMessage(page, 'keyword'),
+			getValidationMessage(page, 'category'),
+		]);
+
+		await expect(page.locator('p')).toHaveText([keywordError, categoryError], {
+			useInnerText: true,
+		});
+
+		expect(keywordError).toBe('');
+		expect(categoryError).not.toBe('');
 
 		await page.locator('select[name="category"]').selectOption('book');
-		await expect(page.locator('p'), 'Category should now be valid').toHaveText(
-			['', ''],
-			{
-				useInnerText: true,
-			},
-		);
+
+		[keywordError, categoryError] = await Promise.all([
+			getValidationMessage(page, 'keyword'),
+			getValidationMessage(page, 'category'),
+		]);
+
+		await expect(page.locator('p')).toHaveText([keywordError, categoryError], {
+			useInnerText: true,
+		});
+
+		expect(keywordError).toBe('');
+		expect(categoryError).toBe('');
 	});
 });
