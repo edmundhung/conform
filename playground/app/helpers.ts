2e9910f2a42be9fe0c154ed5214de27afd73bbf9
Edmund Hung <me@edmund.dev>
2022-07-07 16:53:40 +0200

    chore: introduce touched state to tailwind

--- playground/app/helpers.ts
+++ playground/app/helpers.ts
@@ -5,13 +5,8 @@ export const styles = {
         peer mt-1 block w-full px-3 py-2 bg-white border
         border-slate-300 rounded-md text-sm shadow-sm 
         focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500
-    `,
-	invalidInput: `
-        peer mt-1 block w-full px-3 py-2 bg-white border
-        border-slate-300 rounded-md text-sm shadow-sm 
-        focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500
-        invalid:border-pink-500 invalid:text-pink-600
-        focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+        touched:invalid:border-pink-500 touched:invalid:text-pink-600
+        touched:focus:invalid:border-pink-500 touched:focus:invalid:ring-pink-500
     `,
 	errorMessage: 'my-2 text-pink-600 text-sm peer-valid:invisible',
 };
--- playground/app/routes/index.tsx
+++ playground/app/routes/index.tsx
@@ -85,9 +85,7 @@ function Field({ label, error, children }: FieldProps) {
 	return (
 		<label className="block">
 			<div className={styles.label}>{label}</div>
-			{React.cloneElement(children, {
-				className: error ? styles.invalidInput : styles.input,
-			})}
+			{children}
 			<p className={styles.errorMessage}>{error}</p>
 		</label>
 	);
@@ -129,63 +127,115 @@ function BasicFieldset<T extends string | number | Date | undefined>({
 			<legend>{legend}</legend>
 			<Field label="checkbox" error={checkbox.error}>
 				<input
+					className={styles.input}
 					{...conform.input(checkbox, { type: 'checkbox', value: 'checkbox' })}
 				/>
 			</Field>
 			<Field label="color" error={color.error}>
-				<input {...conform.input(color, { type: 'color' })} />
+				<input
+					className={styles.input}
+					{...conform.input(color, { type: 'color' })}
+				/>
 			</Field>
 			<Field label="date" error={date.error}>
-				<input {...conform.input(date, { type: 'date' })} />
+				<input
+					className={styles.input}
+					{...conform.input(date, { type: 'date' })}
+				/>
 			</Field>
 			<Field label="datetime" error={datetime.error}>
-				<input {...conform.input(datetime, { type: 'datetime-local' })} />
+				<input
+					className={styles.input}
+					{...conform.input(datetime, { type: 'datetime-local' })}
+				/>
 			</Field>
 			<Field label="email" error={email.error}>
-				<input {...conform.input(email, { type: 'email' })} />
+				<input
+					className={styles.input}
+					{...conform.input(email, { type: 'email' })}
+				/>
 			</Field>
 			<Field label="file" error={file.error}>
-				<input {...conform.input(file, { type: 'file' })} />
+				<input
+					className={styles.input}
+					{...conform.input(file, { type: 'file' })}
+				/>
 			</Field>
 			<Field label="hidden" error={hidden.error}>
-				<input {...conform.input(hidden, { type: 'hidden' })} />
+				<input
+					className={styles.input}
+					{...conform.input(hidden, { type: 'hidden' })}
+				/>
 			</Field>
 			<Field label="month" error={month.error}>
-				<input {...conform.input(month, { type: 'month' })} />
+				<input
+					className={styles.input}
+					{...conform.input(month, { type: 'month' })}
+				/>
 			</Field>
 			<Field label="number" error={number.error}>
-				<input {...conform.input(number, { type: 'number' })} />
+				<input
+					className={styles.input}
+					{...conform.input(number, { type: 'number' })}
+				/>
 			</Field>
 			<Field
 				label="password"
 				error={password.error}
 				{...conform.input(password, { type: 'password' })}
 			>
-				<input {...conform.input(password, { type: 'password' })} />
+				<input
+					className={styles.input}
+					{...conform.input(password, { type: 'password' })}
+				/>
 			</Field>
 			<Field label="radio" error={radio.error}>
-				<input {...conform.input(radio, { type: 'radio', value: 'radio' })} />
+				<input
+					className={styles.input}
+					{...conform.input(radio, { type: 'radio', value: 'radio' })}
+				/>
 			</Field>
 			<Field label="range" error={range.error}>
-				<input {...conform.input(range, { type: 'range' })} />
+				<input
+					className={styles.input}
+					{...conform.input(range, { type: 'range' })}
+				/>
 			</Field>
 			<Field label="search" error={search.error}>
-				<input {...conform.input(search, { type: 'search' })} />
+				<input
+					className={styles.input}
+					{...conform.input(search, { type: 'search' })}
+				/>
 			</Field>
 			<Field label="tel" error={tel.error}>
-				<input {...conform.input(tel, { type: 'tel' })} />
+				<input
+					className={styles.input}
+					{...conform.input(tel, { type: 'tel' })}
+				/>
 			</Field>
 			<Field label="text" error={text.error}>
-				<input {...conform.input(text, { type: 'text' })} />
+				<input
+					className={styles.input}
+					{...conform.input(text, { type: 'text' })}
+				/>
 			</Field>
 			<Field label="time" error={time.error}>
-				<input {...conform.input(time, { type: 'time' })} />
+				<input
+					className={styles.input}
+					{...conform.input(time, { type: 'time' })}
+				/>
 			</Field>
 			<Field label="url" error={url.error}>
-				<input {...conform.input(url, { type: 'url' })} />
+				<input
+					className={styles.input}
+					{...conform.input(url, { type: 'url' })}
+				/>
 			</Field>
 			<Field label="week" error={week.error}>
-				<input {...conform.input(week, { type: 'week' })} />
+				<input
+					className={styles.input}
+					{...conform.input(week, { type: 'week' })}
+				/>
 			</Field>
 		</fieldset>
 	);
