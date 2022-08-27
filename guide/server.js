87df9e26094a956fdbd741b408c9b1d7258ada64
Edmund Hung <me@edmund.dev>
2022-08-27 16:10:17 +0200

    chore: bootstrap conform-guide

---
+++ guide/server.js
@@ -0,0 +1,13 @@
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
+
+const handleRequest = createPagesFunctionHandler({
+	build,
+	// eslint-disable-next-line no-undef
+	mode: process.env.NODE_ENV,
+	getLoadContext: (context) => context.env,
+});
+
+export function onRequest(context) {
+	return handleRequest(context);
+}
