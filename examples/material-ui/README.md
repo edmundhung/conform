d5382d908b26e4af9b065fe730b2b45e3e3f12bd
Edmund Hung <me@edmund.dev>
2022-12-07 21:58:29 +0100

    docs(conform-guide): add example for each material-ui component (#53)
    * doc(conform-guide): add example for each material-ui component
    
    * docs: refine layout
    
    * docs: rename title

--- examples/material-ui/README.md
+++ examples/material-ui/README.md
@@ -1,6 +1,25 @@
-# Material UI Example
+# Material UI Integration
 
-This example shows you how to validate non-native input (e.g. Custom dropdown) using the [useControlledInput](/packages/conform-react/README.md#usecontrolledinput) hook.
+[Material UI](https://mui.com/material-ui) is a comprehensive library of components based on Google's Material Design system. In this guide, we will show how to integrate its **Inputs** components with Conform.
+
+## Compatibility
+
+> Based on @mui/material@5.10
+
+| Component             | Native support | Integration required |
+| --------------------- | :------------: | :------------------: |
+| Text Field (input)    |      Yes       |                      |
+| Text Field (textarea) |      Yes       |                      |
+| Select (native)       |      Yes       |                      |
+| Select (default)      |                |         Yes          |
+| Autocomplete          |      Yes       |                      |
+| Checkbox              |      Yes       |                      |
+| Radio Group           |      Yes       |                      |
+| Rating                |                |         Yes          |
+| Slider                |                |         Yes          |
+| Switch                |                |         Yes          |
+
+## Demo
 
 <!-- sandbox src="/examples/material-ui" -->
 
--- examples/material-ui/src/App.tsx
+++ examples/material-ui/src/App.tsx
@@ -1,79 +1,284 @@
+import type { FieldConfig } from '@conform-to/dom';
 import { useForm, useFieldset, useControlledInput } from '@conform-to/react';
-import { TextField, Button, MenuItem, Stack } from '@mui/material';
+import {
+	TextField,
+	Button,
+	MenuItem,
+	Stack,
+	Container,
+	Typography,
+	Autocomplete,
+	Checkbox,
+	FormControl,
+	FormControlLabel,
+	FormGroup,
+	FormHelperText,
+	FormLabel,
+	RadioGroup,
+	Radio,
+	Rating,
+	Slider,
+	Switch,
+} from '@mui/material';
 
-interface Article {
-	title: string;
-	category: string;
-	content: string;
+interface Schema {
+	email: string;
+	description: string;
+	language: string;
+	movie: string;
+	subscribe: boolean;
+	enabled: boolean;
+	active: boolean;
+	score: number;
+	progress: number;
 }
 
 export default function ArticleForm() {
-	const form = useForm<Article>({
-		initialReport: 'onBlur',
-		onSubmit: (event, { submission }) => {
-			event.preventDefault();
+	const form = useForm<Schema>();
+	const fieldset = useFieldset(form.ref, form.config);
 
-			console.log(submission);
-		},
-	});
-	const { title, category, content } = useFieldset(form.ref, form.config);
+	return (
+		<Container maxWidth="sm">
+			<form {...form.props}>
+				<Stack spacing={4} marginY={4}>
+					<header>
+						<Typography variant="h6" component="h1">
+							Material UI Example
+						</Typography>
+						<Typography variant="subtitle1">
+							This example shows you how to integrate Inputs components with
+							Conform.
+						</Typography>
+					</header>
 
+					<TextField
+						label="Email (TextField)"
+						type="email"
+						name="email"
+						error={Boolean(fieldset.email.error)}
+						helperText={fieldset.email.error}
+						required
+					/>
+
+					<TextField
+						label="Description (TextField - multline)"
+						name="description"
+						error={Boolean(fieldset.description.error)}
+						helperText={fieldset.description.error}
+						inputProps={{
+							minLength: 10,
+						}}
+						required
+						multiline
+					/>
+
+					<ExampleSelect
+						label="Language (Select)"
+						name="language"
+						error={fieldset.language.error}
+						required
+					/>
+
+					<ExampleAutocomplete
+						label="Movie (Autocomplete)"
+						name="movie"
+						error={fieldset.movie.error}
+						required
+					/>
+
+					<FormControl
+						component="fieldset"
+						variant="standard"
+						error={Boolean(fieldset.subscribe.error)}
+						required
+					>
+						<FormLabel component="legend">Subscribe (Checkbox)</FormLabel>
+						<FormGroup>
+							<FormControlLabel
+								control={<Checkbox name="subscribe" required />}
+								label="Newsletter"
+							/>
+						</FormGroup>
+						<FormHelperText>{fieldset.subscribe.error}</FormHelperText>
+					</FormControl>
+
+					<FormControl
+						variant="standard"
+						error={Boolean(fieldset.active.error)}
+						required
+					>
+						<FormLabel>Active (Radio)</FormLabel>
+						<RadioGroup name="active">
+							<FormControlLabel
+								value="yes"
+								control={<Radio required />}
+								label="Yes"
+							/>
+							<FormControlLabel
+								value="no"
+								control={<Radio required />}
+								label="No"
+							/>
+						</RadioGroup>
+						<FormHelperText>{fieldset.active.error}</FormHelperText>
+					</FormControl>
+
+					<FormControl
+						variant="standard"
+						error={Boolean(fieldset.enabled.error)}
+						required
+					>
+						<FormLabel>Enabled (Switch)</FormLabel>
+						<FormGroup>
+							<FormControlLabel
+								control={<Switch name="enabled" required />}
+								label="Enabled"
+							/>
+						</FormGroup>
+						<FormHelperText>{fieldset.enabled.error}</FormHelperText>
+					</FormControl>
+
+					<ExampleRating
+						label="Score (Rating)"
+						name="score"
+						error={fieldset.score.error}
+						required
+					/>
+
+					<ExampleSlider
+						label="Progress (Slider)"
+						name="progress"
+						error={fieldset.progress.error}
+						required
+					/>
+
+					<Stack direction="row" justifyContent="flex-end" spacing={2}>
+						<Button type="reset" variant="outlined">
+							Reset
+						</Button>
+						<Button type="submit" variant="contained">
+							Submit
+						</Button>
+					</Stack>
+				</Stack>
+			</form>
+		</Container>
+	);
+}
+
+interface FieldProps<Schema> extends FieldConfig<Schema> {
+	label: string;
+	error?: string;
+}
+
+function ExampleSelect({ label, error, ...config }: FieldProps<string>) {
 	/**
 	 * MUI Select is a non-native input and does not dispatch any DOM events (e.g. input / focus / blur).
 	 * This hooks works by dispatching DOM events manually on the shadow input and thus validated once
 	 * it is hooked up with the controlled component.
 	 */
-	const [categoryInput, control] = useControlledInput(category.config);
+	const [shadowInput, control] = useControlledInput(config);
 
 	return (
-		<form {...form.props}>
-			<Stack spacing={3}>
-				{/* TextField uses native input by default */}
-				<TextField
-					label="title"
-					name="title"
-					error={Boolean(title.error)}
-					helperText={title.error}
-					required
+		<>
+			<input {...shadowInput} />
+			<TextField
+				label={label}
+				inputRef={control.ref}
+				value={control.value}
+				onChange={control.onChange}
+				onBlur={control.onBlur}
+				error={Boolean(error)}
+				helperText={error}
+				inputProps={{
+					// To disable error bubble caused by the constraint
+					// attribute set by mui input, e.g. `required`
+					onInvalid: control.onInvalid,
+				}}
+				select
+				required={config.required}
+			>
+				<MenuItem value="">Please select</MenuItem>
+				<MenuItem value="english">English</MenuItem>
+				<MenuItem value="deutsch">Deutsch</MenuItem>
+				<MenuItem value="japanese">Japanese</MenuItem>
+			</TextField>
+		</>
+	);
+}
+
+function ExampleAutocomplete({ label, error, ...config }: FieldProps<string>) {
+	const [shadowInput, control] = useControlledInput(config);
+	const options = [
+		{ label: 'The Godfather', id: 1 },
+		{ label: 'Pulp Fiction', id: 2 },
+	];
+
+	return (
+		<>
+			<input {...shadowInput} />
+			<Autocomplete
+				disablePortal
+				options={options}
+				value={
+					options.find((option) => control.value === `${option.id}`) ?? null
+				}
+				onChange={(_, option) => control.onChange(`${option?.id ?? ''}`)}
+				renderInput={(params) => (
+					<TextField
+						{...params}
+						label={label}
+						onBlur={control.onBlur}
+						error={Boolean(error)}
+						helperText={error}
+						required={config.required}
+						inputProps={{
+							...params.inputProps,
+							// To disable error bubble caused by the constraint
+							// attribute set by mui input, e.g. `required`
+							onInvalid: control.onInvalid,
+						}}
+					/>
+				)}
+			/>
+		</>
+	);
+}
+
+function ExampleRating({ label, error, ...config }: FieldProps<number>) {
+	const [shadowInput, control] = useControlledInput(config);
+
+	return (
+		<>
+			<input {...shadowInput} />
+			<FormControl variant="standard" error={Boolean(error)} required>
+				<FormLabel>{label}</FormLabel>
+				<Rating
+					ref={control.ref}
+					value={control.value ? Number(control.value) : null}
+					onChange={(_, value) => control.onChange(`${value ?? ''}`)}
 				/>
-				<input {...categoryInput} required />
-				<TextField
-					label="Category"
-					inputRef={control.ref}
-					value={control.value}
-					onChange={control.onChange}
-					onBlur={control.onBlur}
-					error={Boolean(category.error)}
-					helperText={category.error}
-					inputProps={{
-						// To disable error bubble caused by the constraint
-						// attribute set by mui input, e.g. `required`
-						onInvalid: control.onInvalid,
-					}}
-					select
-					required
-				>
-					<MenuItem value="">Please select</MenuItem>
-					<MenuItem value="a">Option A</MenuItem>
-					<MenuItem value="b">Option B</MenuItem>
-					<MenuItem value="c">Option C</MenuItem>
-				</TextField>
-				{/* TextField uses native textarea when multiline is enabled also */}
-				<TextField
-					label="Content"
-					name="content"
-					error={Boolean(content.error)}
-					helperText={content.error}
-					inputProps={{
-						minLength: 10,
-					}}
-					required
-					multiline
+				<FormHelperText>{error}</FormHelperText>
+			</FormControl>
+		</>
+	);
+}
+
+function ExampleSlider({ label, error, ...config }: FieldProps<number>) {
+	const [shadowInput, control] = useControlledInput(config);
+
+	return (
+		<>
+			<input {...shadowInput} />
+			<FormControl variant="standard" error={Boolean(error)} required>
+				<FormLabel>{label}</FormLabel>
+				<Slider
+					ref={control.ref}
+					value={control.value ? Number(control.value) : 0}
+					onChange={(_, value) => control.onChange(`${value}`)}
 				/>
-				<Button type="submit" variant="contained">
-					Submit
-				</Button>
-			</Stack>
-		</form>
+				<FormHelperText>{error}</FormHelperText>
+			</FormControl>
+		</>
 	);
 }
