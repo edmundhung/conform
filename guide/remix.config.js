45ad0f2adc6667c9d05d8a84ee117094993b4ac5
Edmund Hung <me@edmund.dev>
2023-03-05 22:52:08 +0100

    enable v2 route convention

--- guide/remix.config.js
+++ guide/remix.config.js
@@ -1,18 +1,13 @@
-const { flatRoutes } = require('remix-flat-routes');
-
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	serverModuleFormat: 'esm',
 	serverDependenciesToBundle: ['@remix-run/react'],
-	ignoredRouteFiles: ['**/*'],
 	future: {
+		v2_routeConvention: true,
 		unstable_dev: {
 			appServerPort: 3000,
 			rebuildPollIntervalMs: 500,
 		},
 		unstable_tailwind: true,
-	},
-	routes: async (defineRoutes) => {
-		return flatRoutes('routes', defineRoutes);
 	},
 };
