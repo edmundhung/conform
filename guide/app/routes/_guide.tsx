1e126a7ef61103a934eba2196294865407ec0a7d
Edmund Hung <me@edmund.dev>
2023-03-08 15:39:36 +0100

    add header

--- guide/app/routes/_guide.tsx
+++ guide/app/routes/_guide.tsx
@@ -10,7 +10,13 @@ import {
 	useLayoutEffect,
 } from 'react';
 import clsx from 'clsx';
-import { AnimatePresence, motion, useIsPresent } from 'framer-motion';
+import {
+	AnimatePresence,
+	motion,
+	useIsPresent,
+	useScroll,
+	useTransform,
+} from 'framer-motion';
 import { Dialog, Transition } from '@headlessui/react';
 import { create, createStore, useStore } from 'zustand';
 
@@ -291,7 +297,209 @@ export function useSectionStore(selector) {
 	return useStore(store, selector);
 }
 
-// from header.jsx
+// from ModeToggle.jsx
+function SunIcon(props) {
+	return (
+		<svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
+			<path d="M12.5 10a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+			<path
+				strokeLinecap="round"
+				d="M10 5.5v-1M13.182 6.818l.707-.707M14.5 10h1M13.182 13.182l.707.707M10 15.5v-1M6.11 13.889l.708-.707M4.5 10h1M6.11 6.111l.708.707"
+			/>
+		</svg>
+	);
+}
+
+function MoonIcon(props) {
+	return (
+		<svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
+			<path d="M15.224 11.724a5.5 5.5 0 0 1-6.949-6.949 5.5 5.5 0 1 0 6.949 6.949Z" />
+		</svg>
+	);
+}
+
+export function ModeToggle() {
+	function disableTransitionsTemporarily() {
+		document.documentElement.classList.add('[&_*]:!transition-none');
+		window.setTimeout(() => {
+			document.documentElement.classList.remove('[&_*]:!transition-none');
+		}, 0);
+	}
+
+	function toggleMode() {
+		disableTransitionsTemporarily();
+
+		let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+		let isSystemDarkMode = darkModeMediaQuery.matches;
+		let isDarkMode = document.documentElement.classList.toggle('dark');
+
+		if (isDarkMode === isSystemDarkMode) {
+			delete window.localStorage.isDarkMode;
+		} else {
+			window.localStorage.isDarkMode = isDarkMode;
+		}
+	}
+
+	return (
+		<button
+			type="button"
+			className="flex h-6 w-6 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5"
+			aria-label="Toggle dark mode"
+			onClick={toggleMode}
+		>
+			<SunIcon className="h-5 w-5 stroke-zinc-900 dark:hidden" />
+			<MoonIcon className="hidden h-5 w-5 stroke-white dark:block" />
+		</button>
+	);
+}
+
+// from Search.jsx
+function useSearchProps() {
+	let buttonRef = useRef();
+	let [open, setOpen] = useState(false);
+
+	return {
+		buttonProps: {
+			ref: buttonRef,
+			onClick() {
+				setOpen(true);
+			},
+		},
+		dialogProps: {
+			open,
+			setOpen(open) {
+				let { width, height } = buttonRef.current.getBoundingClientRect();
+				if (!open || (width !== 0 && height !== 0)) {
+					setOpen(open);
+				}
+			},
+		},
+	};
+}
+
+export function Search() {
+	let [modifierKey, setModifierKey] = useState();
+	let { buttonProps, dialogProps } = useSearchProps();
+
+	useEffect(() => {
+		setModifierKey(
+			/(Mac|iPhone|iPod|iPad)/i.test(navigator.platform) ? '⌘' : 'Ctrl ',
+		);
+	}, []);
+
+	return (
+		<div className="hidden lg:block lg:max-w-md lg:flex-auto">
+			<button
+				type="button"
+				className="hidden h-8 w-full items-center gap-2 rounded-full bg-white pl-2 pr-3 text-sm text-zinc-500 ring-1 ring-zinc-900/10 transition hover:ring-zinc-900/20 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:ring-white/20 lg:flex focus:[&:not(:focus-visible)]:outline-none"
+				{...buttonProps}
+			>
+				{/* <SearchIcon className="h-5 w-5 stroke-current" /> */}
+				Find something...
+				<kbd className="ml-auto text-2xs text-zinc-400 dark:text-zinc-500">
+					<kbd className="font-sans">{modifierKey}</kbd>
+					<kbd className="font-sans">K</kbd>
+				</kbd>
+			</button>
+			{/* <SearchDialog className="hidden lg:block" {...dialogProps} /> */}
+		</div>
+	);
+}
+
+export function MobileSearch() {
+	let { buttonProps, dialogProps } = useSearchProps();
+
+	return (
+		<div className="contents lg:hidden">
+			<button
+				type="button"
+				className="flex h-6 w-6 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5 lg:hidden focus:[&:not(:focus-visible)]:outline-none"
+				aria-label="Find something..."
+				{...buttonProps}
+			>
+				{/* <SearchIcon className="h-5 w-5 stroke-zinc-900 dark:stroke-white" /> */}
+			</button>
+			{/* <SearchDialog className="lg:hidden" {...dialogProps} /> */}
+		</div>
+	);
+}
+
+// from Header.jsx
+function TopLevelNavItemHeader({ href, children }) {
+	return (
+		<li>
+			<Link
+				to={href}
+				className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+			>
+				{children}
+			</Link>
+		</li>
+	);
+}
+
+export const Header = forwardRef(function Header({ className }, ref) {
+	let { isOpen: mobileNavIsOpen } = useMobileNavigationStore();
+	let isInsideMobileNavigation = useIsInsideMobileNavigation();
+
+	let { scrollY } = useScroll();
+	let bgOpacityLight = useTransform(scrollY, [0, 72], [0.5, 0.9]);
+	let bgOpacityDark = useTransform(scrollY, [0, 72], [0.2, 0.8]);
+
+	return (
+		<motion.div
+			ref={ref}
+			className={clsx(
+				className,
+				'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
+				!isInsideMobileNavigation &&
+					'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
+				isInsideMobileNavigation
+					? 'bg-white dark:bg-zinc-900'
+					: 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]',
+			)}
+			style={{
+				'--bg-opacity-light': bgOpacityLight,
+				'--bg-opacity-dark': bgOpacityDark,
+			}}
+		>
+			<div
+				className={clsx(
+					'absolute inset-x-0 top-full h-px transition',
+					(isInsideMobileNavigation || !mobileNavIsOpen) &&
+						'bg-zinc-900/7.5 dark:bg-white/7.5',
+				)}
+			/>
+			<Search />
+			<div className="flex items-center gap-5 lg:hidden">
+				<MobileNavigation />
+				<Link to="/" aria-label="Home">
+					{/* <Logo className="h-6" /> */}
+					Conform
+				</Link>
+			</div>
+			<div className="flex items-center gap-5">
+				<nav className="hidden md:block">
+					<ul role="list" className="flex items-center gap-8">
+						<TopLevelNavItemHeader href="/">API</TopLevelNavItemHeader>
+						<TopLevelNavItemHeader href="#">
+							Documentation
+						</TopLevelNavItemHeader>
+						<TopLevelNavItemHeader href="#">Support</TopLevelNavItemHeader>
+					</ul>
+				</nav>
+				<div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
+				<div className="flex gap-4">
+					<MobileSearch />
+					<ModeToggle />
+				</div>
+				<div className="hidden min-[416px]:contents">
+					<Button href="#">Sign in</Button>
+				</div>
+			</div>
+		</motion.div>
+	);
+});
 
 // from MobileNavigation.jsx
 function MenuIcon(props) {
@@ -375,7 +583,7 @@ export function MobileNavigation() {
 								leaveFrom="opacity-100"
 								leaveTo="opacity-0"
 							>
-								{/* <Header /> */}
+								<Header />
 							</Transition.Child>
 
 							<Transition.Child
@@ -622,7 +830,7 @@ export default function Guide() {
 								<Link aria-label="Home" to="/">
 									Conform
 								</Link>
-								{/* <Header /> */}
+								<Header />
 								<Navigation className="hidden lg:mt-10 lg:block" />
 							</div>
 						</div>
