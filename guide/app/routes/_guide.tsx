29c1461c69e42f23010e9584bca8f5f73711a319
Edmund Hung <me@edmund.dev>
2023-03-18 18:17:31 +0100

    flatten footer structure

--- guide/app/routes/_guide.tsx
+++ guide/app/routes/_guide.tsx
@@ -1253,7 +1253,7 @@ function NavigationGroup({ group, className }) {
 						<ActivePageMarker group={group} pathname={location.pathname} />
 					)}
 				</AnimatePresence>
-				<ul role="list" className="border-l border-transparent">
+				<ul className="border-l border-transparent">
 					{group.menus.map((link) => (
 						<motion.li key={link.to} layout="position" className="relative">
 							<NavLink href={link.to} active={link.to === location.pathname}>
@@ -1298,7 +1298,7 @@ function NavigationGroup({ group, className }) {
 export function Navigation(props) {
 	return (
 		<nav {...props}>
-			<ul role="list">
+			<ul>
 				<TopLevelNavItem href="/">API</TopLevelNavItem>
 				<TopLevelNavItem href="#">Documentation</TopLevelNavItem>
 				<TopLevelNavItem href="#">Support</TopLevelNavItem>
@@ -1310,107 +1310,16 @@ export function Navigation(props) {
 					/>
 				))}
 				<li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-					<Button to="#" variant="filled" className="w-full">
-						Sign in
+					<Button
+						to="https://github.com/edmundhung/conform"
+						variant="filled"
+						className="w-full"
+					>
+						GitHub
 					</Button>
 				</li>
 			</ul>
 		</nav>
-	);
-}
-
-// from Footer.jsx
-function CheckIcon(props) {
-	return (
-		<svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
-			<circle cx="10" cy="10" r="10" strokeWidth="0" />
-			<path
-				fill="none"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-				strokeWidth="1.5"
-				d="m6.75 10.813 2.438 2.437c1.218-4.469 4.062-6.5 4.062-6.5"
-			/>
-		</svg>
-	);
-}
-
-function FeedbackButton(props) {
-	return (
-		<button
-			type="submit"
-			className="px-3 text-sm font-medium text-zinc-600 transition hover:bg-zinc-900/2.5 hover:text-zinc-900 dark:text-zinc-400 dark:hover:bg-white/5 dark:hover:text-white"
-			{...props}
-		/>
-	);
-}
-
-const FeedbackForm = forwardRef(function FeedbackForm({ onSubmit }, ref) {
-	return (
-		<form
-			ref={ref}
-			onSubmit={onSubmit}
-			className="absolute inset-0 flex items-center justify-center gap-6 md:justify-start"
-		>
-			<p className="text-sm text-zinc-600 dark:text-zinc-400">
-				Was this page helpful?
-			</p>
-			<div className="group grid h-8 grid-cols-[1fr,1px,1fr] overflow-hidden rounded-full border border-zinc-900/10 dark:border-white/10">
-				<FeedbackButton data-response="yes">Yes</FeedbackButton>
-				<div className="bg-zinc-900/10 dark:bg-white/10" />
-				<FeedbackButton data-response="no">No</FeedbackButton>
-			</div>
-		</form>
-	);
-});
-
-const FeedbackThanks = forwardRef(function FeedbackThanks(_props, ref) {
-	return (
-		<div
-			ref={ref}
-			className="absolute inset-0 flex justify-center md:justify-start"
-		>
-			<div className="flex items-center gap-3 rounded-full bg-emerald-50/50 py-1 pr-3 pl-1.5 text-sm text-emerald-900 ring-1 ring-inset ring-emerald-500/20 dark:bg-emerald-500/5 dark:text-emerald-200 dark:ring-emerald-500/30">
-				<CheckIcon className="h-5 w-5 flex-none fill-emerald-500 stroke-white dark:fill-emerald-200/20 dark:stroke-emerald-200" />
-				Thanks for your feedback!
-			</div>
-		</div>
-	);
-});
-
-function Feedback() {
-	let [submitted, setSubmitted] = useState(false);
-
-	function onSubmit(event) {
-		event.preventDefault();
-
-		// event.nativeEvent.submitter.dataset.response
-		// => "yes" or "no"
-
-		setSubmitted(true);
-	}
-
-	return (
-		<div className="relative h-8">
-			<Transition
-				show={!submitted}
-				as={Fragment}
-				leaveFrom="opacity-100"
-				leaveTo="opacity-0"
-				leave="pointer-events-none duration-300"
-			>
-				<FeedbackForm onSubmit={onSubmit} />
-			</Transition>
-			<Transition
-				show={submitted}
-				as={Fragment}
-				enterFrom="opacity-0"
-				enterTo="opacity-100"
-				enter="delay-150 duration-300"
-			>
-				<FeedbackThanks />
-			</Transition>
-		</div>
 	);
 }
 
@@ -1437,84 +1346,54 @@ function PageLink({ label, page, previous = false }) {
 	);
 }
 
-function PageNavigation() {
-	let location = useLocation();
-	let allPages = navigation.flatMap((group) => group.menus);
-	let currentPageIndex = allPages.findIndex(
+export function Footer() {
+	const location = useLocation();
+	const allPages = navigation.flatMap((group) => group.menus);
+	const currentPageIndex = allPages.findIndex(
 		(page) => page.to === location.pathname,
 	);
-
-	if (currentPageIndex === -1) {
-		return null;
-	}
-
-	let previousPage = allPages[currentPageIndex - 1];
-	let nextPage = allPages[currentPageIndex + 1];
-
-	if (!previousPage && !nextPage) {
-		return null;
-	}
-
-	return (
-		<div className="flex">
-			{previousPage && (
-				<div className="flex flex-col items-start gap-3">
-					<PageLink label="Previous" page={previousPage} previous />
-				</div>
-			)}
-			{nextPage && (
-				<div className="ml-auto flex flex-col items-end gap-3">
-					<PageLink label="Next" page={nextPage} />
-				</div>
-			)}
-		</div>
-	);
-}
-
-function GitHubIcon(props) {
-	return (
-		<svg viewBox="0 0 20 20" aria-hidden="true" {...props}>
-			<path
-				fillRule="evenodd"
-				clipRule="evenodd"
-				d="M10 1.667c-4.605 0-8.334 3.823-8.334 8.544 0 3.78 2.385 6.974 5.698 8.106.417.075.573-.182.573-.406 0-.203-.011-.875-.011-1.592-2.093.397-2.635-.522-2.802-1.002-.094-.246-.5-1.005-.854-1.207-.291-.16-.708-.556-.01-.567.656-.01 1.124.62 1.281.876.75 1.292 1.948.93 2.427.705.073-.555.291-.93.531-1.143-1.854-.213-3.791-.95-3.791-4.218 0-.929.322-1.698.854-2.296-.083-.214-.375-1.09.083-2.265 0 0 .698-.224 2.292.876a7.576 7.576 0 0 1 2.083-.288c.709 0 1.417.096 2.084.288 1.593-1.11 2.291-.875 2.291-.875.459 1.174.167 2.05.084 2.263.53.599.854 1.357.854 2.297 0 3.278-1.948 4.005-3.802 4.219.302.266.563.78.563 1.58 0 1.143-.011 2.061-.011 2.35 0 .224.156.491.573.405a8.365 8.365 0 0 0 4.11-3.116 8.707 8.707 0 0 0 1.567-4.99c0-4.721-3.73-8.545-8.334-8.545Z"
-			/>
-		</svg>
-	);
-}
-
-function SocialLink({ href, icon: Icon, children }) {
-	return (
-		<Link to={href} className="group">
-			<span className="sr-only">{children}</span>
-			<Icon className="h-5 w-5 fill-zinc-700 transition group-hover:fill-zinc-900 dark:group-hover:fill-zinc-500" />
-		</Link>
-	);
-}
-
-function SmallPrint() {
-	return (
-		<div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
-			<p className="text-xs text-zinc-600 dark:text-zinc-400">
-				&copy; Copyright {new Date().getFullYear()}. All rights reserved.
-			</p>
-			<div className="flex gap-4">
-				<SocialLink href="#" icon={GitHubIcon}>
-					Follow us on GitHub
-				</SocialLink>
-			</div>
-		</div>
-	);
-}
-
-export function Footer() {
-	let location = useLocation();
+	const previousPage =
+		currentPageIndex > -1 ? allPages[currentPageIndex - 1] : undefined;
+	const nextPage =
+		currentPageIndex > -1 ? allPages[currentPageIndex + 1] : undefined;
 
 	return (
 		<footer className="mx-auto max-w-2xl space-y-10 pb-16 lg:max-w-5xl">
-			<Feedback key={location.pathname} />
-			<PageNavigation />
-			<SmallPrint />
+			{previousPage || nextPage ? (
+				<div className="flex">
+					{previousPage && (
+						<div className="flex flex-col items-start gap-3">
+							<PageLink label="Previous" page={previousPage} previous />
+						</div>
+					)}
+					{nextPage && (
+						<div className="ml-auto flex flex-col items-end gap-3">
+							<PageLink label="Next" page={nextPage} />
+						</div>
+					)}
+				</div>
+			) : null}
+			<div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
+				<p className="text-xs text-zinc-600 dark:text-zinc-400">
+					&copy; Copyright {new Date().getFullYear()}. All rights reserved.
+				</p>
+				<div className="flex gap-4">
+					<Link to="https://github.com/edmundhung/conform" className="group">
+						<span className="sr-only">Follow us on GitHub</span>
+						<svg
+							className="h-5 w-5 fill-zinc-700 transition group-hover:fill-zinc-900 dark:group-hover:fill-zinc-500"
+							viewBox="0 0 20 20"
+							aria-hidden="true"
+						>
+							<path
+								fillRule="evenodd"
+								clipRule="evenodd"
+								d="M10 1.667c-4.605 0-8.334 3.823-8.334 8.544 0 3.78 2.385 6.974 5.698 8.106.417.075.573-.182.573-.406 0-.203-.011-.875-.011-1.592-2.093.397-2.635-.522-2.802-1.002-.094-.246-.5-1.005-.854-1.207-.291-.16-.708-.556-.01-.567.656-.01 1.124.62 1.281.876.75 1.292 1.948.93 2.427.705.073-.555.291-.93.531-1.143-1.854-.213-3.791-.95-3.791-4.218 0-.929.322-1.698.854-2.296-.083-.214-.375-1.09.083-2.265 0 0 .698-.224 2.292.876a7.576 7.576 0 0 1 2.083-.288c.709 0 1.417.096 2.084.288 1.593-1.11 2.291-.875 2.291-.875.459 1.174.167 2.05.084 2.263.53.599.854 1.357.854 2.297 0 3.278-1.948 4.005-3.802 4.219.302.266.563.78.563 1.58 0 1.143-.011 2.061-.011 2.35 0 .224.156.491.573.405a8.365 8.365 0 0 0 4.11-3.116 8.707 8.707 0 0 0 1.567-4.99c0-4.721-3.73-8.545-8.334-8.545Z"
+							/>
+						</svg>
+					</Link>
+				</div>
+			</div>
 		</footer>
 	);
 }
