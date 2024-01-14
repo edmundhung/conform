9e23b770923ad16725e0e80e54729f79817f7d30
Matt Mazzola <mattmazzola@live.com>
2024-01-14 17:26:42 +0000

    Add /favicon.ico and links

--- guide/app/root.tsx
+++ guide/app/root.tsx
@@ -17,7 +17,27 @@ import stylesUrl from '~/styles.css';
 import { getBranch } from './context';
 
 export let links: LinksFunction = () => {
-	return [{ rel: 'stylesheet', href: stylesUrl }];
+	return [
+		{ rel: 'stylesheet', href: stylesUrl },
+		{
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			href: '/apple-touch-icon.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '32x32',
+			href: '/favicon-32x32.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '16x16',
+			href: '/favicon-16x16.png',
+		},
+		{ rel: 'manifest', href: '/site.webmanifest' },
+	];
 };
 
 export function loader({ context }: LoaderArgs) {
