f938292aa23d6df169e74f74c05eb213463f247f
Edmund Hung <me@edmund.dev>
2024-02-04 21:41:12 +0100

    fix(conform-react): useInputControl should populate initial value and support mult select

--- packages/conform-react/integrations.ts
+++ packages/conform-react/integrations.ts
@@ -1,85 +1,204 @@
-import { type FieldElement, isFieldElement } from '@conform-to/dom';
 import { type Key, useRef, useState, useMemo, useEffect } from 'react';
 
-export type InputControl = {
-	value: string | undefined;
-	change: (value: string) => void;
+export type InputControl<Value> = {
+	value: Value | undefined;
+	change: (value: Value) => void;
 	focus: () => void;
 	blur: () => void;
 };
 
-export function getFieldElement(
-	formId: string,
+export function getFormElement(formId: string): HTMLFormElement {
+	const element = document.forms.namedItem(formId);
+
+	if (!element) {
+		throw new Error('Form not found');
+	}
+
+	return element;
+}
+
+export function getFieldElements(
+	form: HTMLFormElement,
 	name: string,
-	match: (element: FieldElement) => boolean = () => true,
-): FieldElement | null {
-	const element = document.forms.namedItem(formId)?.elements.namedItem(name);
+): Array<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement> {
+	const field = form.elements.namedItem(name);
+	const elements = !field
+		? []
+		: field instanceof Element
+		? [field]
+		: Array.from(field.values());
 
-	if (element) {
-		const items =
-			element instanceof Element ? [element] : Array.from(element.values());
+	return elements.filter(
+		(
+			element,
+		): element is HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement =>
+			element instanceof HTMLInputElement ||
+			element instanceof HTMLSelectElement ||
+			element instanceof HTMLTextAreaElement,
+	);
+}
 
-		for (const item of items) {
-			if (isFieldElement(item) && match(item)) {
-				return item;
+export function getEventTarget(
+	form: HTMLFormElement,
+	name: string,
+): HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement | null {
+	const elements = getFieldElements(form, name);
+
+	return elements[0] ?? null;
+}
+
+export function createDummySelect(
+	form: HTMLFormElement,
+	name: string,
+	value?: string | string[] | undefined,
+): HTMLSelectElement {
+	const select = document.createElement('select');
+	const options = Array.isArray(value) ? value : [value ?? ''];
+
+	select.name = name;
+	select.multiple = true;
+	select.dataset.conform = 'true';
+
+	// To make sure the input is hidden but still focusable
+	select.setAttribute('aria-hidden', 'true');
+	select.tabIndex = -1;
+	select.style.position = 'absolute';
+	select.style.width = '1px';
+	select.style.height = '1px';
+	select.style.padding = '0';
+	select.style.margin = '-1px';
+	select.style.overflow = 'hidden';
+	select.style.clip = 'rect(0,0,0,0)';
+	select.style.whiteSpace = 'nowrap';
+	select.style.border = '0';
+
+	for (const option of options) {
+		select.options.add(new Option(option, option, true, true));
+	}
+
+	form.appendChild(select);
+
+	return select;
+}
+
+export function isDummySelect(
+	element: HTMLElement,
+): element is HTMLSelectElement {
+	return element.dataset.conform === 'true';
+}
+
+export function updateFieldValue(
+	element: HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement,
+	value: string | string[],
+): void {
+	if (
+		element instanceof HTMLInputElement &&
+		(element.type === 'checkbox' || element.type === 'radio')
+	) {
+		element.checked = element.value === value;
+	} else if (element instanceof HTMLSelectElement && element.multiple) {
+		const selectedValue = Array.isArray(value) ? [...value] : [value];
+
+		for (const option of element.options) {
+			const index = selectedValue.indexOf(option.value);
+			const selected = index > -1;
+
+			// Update the selected state of the option
+			option.selected = selected;
+			// Remove the option from the selected array
+			if (selected) {
+				selectedValue.splice(index, 1);
+			}
+		}
+
+		// Add the remaining options to the select element only if it's a dummy element managed by conform
+		if (isDummySelect(element)) {
+			for (const option of selectedValue) {
+				element.options.add(new Option(option, option, false, true));
+			}
+		}
+	} else if (element.value !== value) {
+		// No `change` event will be triggered on React if `element.value` is already updated
+
+		/**
+		 * Triggering react custom change event
+		 * Solution based on dom-testing-library
+		 * @see https://github.com/facebook/react/issues/10135#issuecomment-401496776
+		 * @see https://github.com/testing-library/dom-testing-library/blob/main/src/events.js#L104-L123
+		 */
+		const { set: valueSetter } =
+			Object.getOwnPropertyDescriptor(element, 'value') || {};
+		const prototype = Object.getPrototypeOf(element);
+		const { set: prototypeValueSetter } =
+			Object.getOwnPropertyDescriptor(prototype, 'value') || {};
+
+		if (prototypeValueSetter && valueSetter !== prototypeValueSetter) {
+			prototypeValueSetter.call(element, value);
+		} else {
+			if (valueSetter) {
+				valueSetter.call(element, value);
+			} else {
+				throw new Error('The given element does not have a value setter');
 			}
 		}
 	}
-
-	return null;
 }
 
-export function getEventTarget(formId: string, name: string): FieldElement {
-	const element = getFieldElement(formId, name);
-
-	if (element) {
-		return element;
-	}
-
-	const form = document.forms.namedItem(formId);
-	const input = document.createElement('input');
-
-	input.type = 'hidden';
-	input.name = name;
-
-	form?.appendChild(input);
-
-	return input;
-}
-
-export type InputControlOptions = {
+export function useInputControl<
+	Value extends string | string[],
+>(metaOrOptions: {
 	key?: Key | null | undefined;
 	name: string;
 	formId: string;
-	initialValue?: string | undefined;
-};
-
-export function useInputControl(
-	metaOrOptions: InputControlOptions,
-): InputControl {
+	initialValue?: Value | undefined;
+}): InputControl<Value> {
 	const eventDispatched = useRef({
 		change: false,
 		focus: false,
 		blur: false,
 	});
 	const [key, setKey] = useState(metaOrOptions.key);
-	const [value, setValue] = useState(() => metaOrOptions.initialValue);
+	const [initialValue, setInitialValue] = useState(metaOrOptions.initialValue);
+	const [value, setValue] = useState(metaOrOptions.initialValue);
 
 	if (key !== metaOrOptions.key) {
 		setValue(metaOrOptions.initialValue);
+		setInitialValue(metaOrOptions.initialValue);
 		setKey(metaOrOptions.key);
 	}
 
 	useEffect(() => {
+		const form = getFormElement(metaOrOptions.formId);
+
+		if (getEventTarget(form, metaOrOptions.name)) {
+			return;
+		}
+
+		createDummySelect(form, metaOrOptions.name, initialValue);
+
+		return () => {
+			const elements = getFieldElements(form, metaOrOptions.name);
+
+			for (const element of elements) {
+				if (isDummySelect(element)) {
+					element.remove();
+				}
+			}
+		};
+	}, [metaOrOptions.formId, metaOrOptions.name, initialValue]);
+
+	useEffect(() => {
 		const createEventListener = (listener: 'change' | 'focus' | 'blur') => {
 			return (event: Event) => {
-				const element = getFieldElement(
-					metaOrOptions.formId,
-					metaOrOptions.name,
-					(element) => element === event.target,
-				);
+				const element = event.target;
 
-				if (element) {
+				if (
+					(element instanceof HTMLInputElement ||
+						element instanceof HTMLSelectElement ||
+						element instanceof HTMLTextAreaElement) &&
+					element.name === metaOrOptions.name &&
+					element.form?.id === metaOrOptions.formId
+				) {
 					eventDispatched.current[listener] = true;
 				}
 			};
@@ -99,92 +218,62 @@ export function useInputControl(
 		};
 	}, [metaOrOptions.formId, metaOrOptions.name]);
 
-	const handlers = useMemo<Omit<InputControl, 'value'>>(() => {
+	const handlers = useMemo<Omit<InputControl<Value>, 'value'>>(() => {
 		return {
 			change(value) {
 				if (!eventDispatched.current.change) {
-					const element = getEventTarget(
-						metaOrOptions.formId,
-						metaOrOptions.name,
-					);
-
 					eventDispatched.current.change = true;
 
-					if (
-						element instanceof HTMLInputElement &&
-						(element.type === 'checkbox' || element.type === 'radio')
-					) {
-						element.checked = element.value === value;
-					} else if (element.value !== value) {
-						// No change event will be triggered on React if `element.value` is already updated
+					const form = getFormElement(metaOrOptions.formId);
+					const element = getEventTarget(form, metaOrOptions.name);
 
-						/**
-						 * Triggering react custom change event
-						 * Solution based on dom-testing-library
-						 * @see https://github.com/facebook/react/issues/10135#issuecomment-401496776
-						 * @see https://github.com/testing-library/dom-testing-library/blob/main/src/events.js#L104-L123
-						 */
-						const { set: valueSetter } =
-							Object.getOwnPropertyDescriptor(element, 'value') || {};
-						const prototype = Object.getPrototypeOf(element);
-						const { set: prototypeValueSetter } =
-							Object.getOwnPropertyDescriptor(prototype, 'value') || {};
+					if (element) {
+						updateFieldValue(element, value);
 
-						if (prototypeValueSetter && valueSetter !== prototypeValueSetter) {
-							prototypeValueSetter.call(element, value);
-						} else {
-							if (valueSetter) {
-								valueSetter.call(element, value);
-							} else {
-								throw new Error(
-									'The given element does not have a value setter',
-								);
-							}
-						}
+						// Dispatch input event with the updated input value
+						element.dispatchEvent(new InputEvent('input', { bubbles: true }));
+						// Dispatch change event (necessary for select to update the selected option)
+						element.dispatchEvent(new Event('change', { bubbles: true }));
 					}
-
-					// Dispatch input event with the updated input value
-					element.dispatchEvent(new InputEvent('input', { bubbles: true }));
-					// Dispatch change event (necessary for select to update the selected option)
-					element.dispatchEvent(new Event('change', { bubbles: true }));
 				}
 
 				setValue(value);
-
 				eventDispatched.current.change = false;
 			},
 			focus() {
 				if (!eventDispatched.current.focus) {
-					const element = getEventTarget(
-						metaOrOptions.formId,
-						metaOrOptions.name,
-					);
-
 					eventDispatched.current.focus = true;
-					element.dispatchEvent(
-						new FocusEvent('focusin', {
-							bubbles: true,
-						}),
-					);
-					element.dispatchEvent(new FocusEvent('focus'));
+
+					const form = getFormElement(metaOrOptions.formId);
+					const element = getEventTarget(form, metaOrOptions.name);
+
+					if (element) {
+						element.dispatchEvent(
+							new FocusEvent('focusin', {
+								bubbles: true,
+							}),
+						);
+						element.dispatchEvent(new FocusEvent('focus'));
+					}
 				}
 
 				eventDispatched.current.focus = false;
 			},
 			blur() {
 				if (!eventDispatched.current.blur) {
-					const element = getEventTarget(
-						metaOrOptions.formId,
-						metaOrOptions.name,
-					);
-
 					eventDispatched.current.blur = true;
-					element.dispatchEvent(
-						new FocusEvent('focusout', {
-							bubbles: true,
-						}),
-					);
-					element.dispatchEvent(new FocusEvent('blur'));
+
+					const form = getFormElement(metaOrOptions.formId);
+					const element = getEventTarget(form, metaOrOptions.name);
+
+					if (element) {
+						element.dispatchEvent(
+							new FocusEvent('focusout', {
+								bubbles: true,
+							}),
+						);
+						element.dispatchEvent(new FocusEvent('blur'));
+					}
 				}
 
 				eventDispatched.current.blur = false;
