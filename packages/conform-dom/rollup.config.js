2cb4dd56d2f4dc476200509b1b81953a96e27d34
Aaron Reisman <aaron@lifeiscontent.net>
2024-04-14 13:21:56 -0700

    chore: refactor rollup

---
+++ packages/conform-dom/rollup.config.js
@@ -0,0 +1,100 @@
+import path from 'node:path';
+import babel from '@rollup/plugin-babel';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+
+/** @returns {import("rollup").RollupOptions[]} */
+function configurePackage() {
+	let sourceDir = '.';
+	let outputDir = sourceDir;
+
+	/** @type {import("rollup").RollupOptions} */
+	let ESM = {
+		external(id) {
+			return !id.startsWith('.') && !path.isAbsolute(id);
+		},
+		input: `${sourceDir}/index.ts`,
+		output: {
+			dir: outputDir,
+			format: 'esm',
+			preserveModules: true,
+			entryFileNames: '[name].mjs',
+		},
+		plugins: [
+			babel({
+				babelrc: false,
+				configFile: false,
+				presets: [
+					[
+						'@babel/preset-env',
+						{
+							targets: {
+								node: '16',
+								esmodules: true,
+							},
+						},
+					],
+					'@babel/preset-typescript',
+				],
+				plugins: [],
+				babelHelpers: 'bundled',
+				exclude: /node_modules/,
+				extensions: ['.ts', '.tsx'],
+			}),
+			nodeResolve({
+				extensions: ['.ts', '.tsx'],
+			}),
+			copy({
+				targets: [
+					{ src: `../../README`, dest: sourceDir },
+					{ src: `../../LICENSE`, dest: sourceDir },
+				],
+			}),
+		],
+	};
+
+	/** @type {import("rollup").RollupOptions} */
+	let CJS = {
+		external(id) {
+			return !id.startsWith('.') && !path.isAbsolute(id);
+		},
+		input: `${sourceDir}/index.ts`,
+		output: {
+			dir: outputDir,
+			format: 'cjs',
+			preserveModules: true,
+			exports: 'auto',
+		},
+		plugins: [
+			babel({
+				babelrc: false,
+				configFile: false,
+				presets: [
+					[
+						'@babel/preset-env',
+						{
+							targets: {
+								node: '16',
+								esmodules: true,
+							},
+						},
+					],
+					'@babel/preset-typescript',
+				],
+				plugins: [],
+				babelHelpers: 'bundled',
+				exclude: /node_modules/,
+				extensions: ['.ts', '.tsx'],
+			}),
+			nodeResolve({
+				extensions: ['.ts', '.tsx'],
+			}),
+		],
+	};
+
+	return [ESM, CJS];
+}
+
+export default function rollup() {
+	return configurePackage();
+}
