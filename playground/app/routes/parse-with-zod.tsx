9b1c275bd5a01fdfb2e08cd25d051d9b7ff3aa57
Aaron Reisman <aaronreisman@sort.xyz>
2024-06-17 17:51:03 -0700

    chore: add json example

---
+++ playground/app/routes/parse-with-zod.tsx
@@ -0,0 +1,74 @@
+import { getFormProps, getInputProps, useForm } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
+import {
+	type LoaderFunctionArgs,
+	type ActionFunctionArgs,
+	json,
+} from '@remix-run/node';
+import { Form, useActionData, useLoaderData } from '@remix-run/react';
+import { Playground, Field } from '~/components';
+import { z } from 'zod';
+
+const literalSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonSchema: z.ZodType<Json> = z.lazy(() =>
+	z.union([literalSchema, z.array(jsonSchema), z.record(jsonSchema)]),
+);
+
+const schema = z.object({
+	username: z.string().min(3, 'Username is too short'),
+	profile: jsonSchema,
+});
+
+export async function loader({ request }: LoaderFunctionArgs) {
+	const url = new URL(request.url);
+
+	return {
+		noClientValidate: url.searchParams.get('noClientValidate') === 'yes',
+	};
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	const formData = await request.formData();
+	const submission = parseWithZod(formData, {
+		schema,
+	});
+
+	if (submission.status === 'success') {
+		// eslint-disable-next-line no-console
+		console.log(submission.value);
+	}
+
+	return json(submission.reply());
+}
+
+export default function Example() {
+	const { noClientValidate } = useLoaderData<typeof loader>();
+	const lastResult = useActionData<typeof action>();
+	const [form, fields] = useForm({
+		lastResult,
+		defaultValue: {
+			username: 'test',
+			profile: {
+				age: 35,
+			},
+		},
+		onValidate: !noClientValidate
+			? ({ formData }) => parseWithZod(formData, { schema })
+			: undefined,
+	});
+
+	return (
+		<Form method="post" {...getFormProps(form)}>
+			<Playground title="Parse with zod" result={lastResult}>
+				<Field label="Username" meta={fields.username}>
+					<input {...getInputProps(fields.username, { type: 'text' })} />
+				</Field>
+				<Field label="Profile" meta={fields.profile}>
+					<input {...getInputProps(fields.profile, { type: 'text' })} />
+				</Field>
+			</Playground>
+		</Form>
+	);
+}
