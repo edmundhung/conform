dd6233035e9e5acf40b4f754d33284fad54a602d
Edmund Hung <me@edmund.dev>
2024-09-13 23:46:19 +0100

    chore: auto update readme version (#765)

---
+++ .github/version.sh
@@ -0,0 +1,5 @@
+# Find the current version of the @conform-to/dom package
+VERSION=$(node -p "require('./packages/conform-dom/package.json').version")
+
+# Replace the version on the README
+sed -i '' "s/^Version [0-9]*\.[0-9]*\.[0-9]*/Version ${VERSION}/" README.md
