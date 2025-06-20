65b2374a2a072a473b62f739ba75269b6f43bbc1
chimame <rito.tamata@gmail.com>
2025-06-21 00:32:38 +0900

    fix(conform-zod): coercion for composed discriminated unions (#969)

--- packages/conform-zod/v4/coercion.ts
+++ packages/conform-zod/v4/coercion.ts
@@ -1,4 +1,9 @@
-import type { $ZodObjectDef, $ZodType, $ZodTypes } from 'zod/v4/core';
+import type {
+	$ZodDiscriminatedUnionDef,
+	$ZodObjectDef,
+	$ZodType,
+	$ZodTypes,
+} from 'zod/v4/core';
 import { lazy, pipe, transform } from 'zod/v4-mini';
 
 /**
@@ -274,7 +279,23 @@ export function enableTypeCoercion<Schema extends $ZodType>(
 			new constr({
 				...def,
 				options: def.options.map((item) => {
-					const objectDef = item._zod.def as $ZodObjectDef;
+					const objectDef = item._zod.def as
+						| $ZodObjectDef
+						| $ZodDiscriminatedUnionDef;
+					const setOriginalPropValues = (
+						object: $ZodType<unknown, unknown>,
+					) => {
+						// The discriminate key is obtained from the defined Object.
+						// If you regenerate the Object schema, the `propValues` property disappears. Therefore, set the one obtained from the original Object.
+						// https://github.com/colinhacks/zod/blob/22ab436bc214d86d740e78f33ae6834d28ddc152/packages/zod/src/v4/core/schemas.ts#L1949-L1963
+						object._zod.propValues = item._zod.propValues;
+						// @ts-expect-error: The `disc` property was used up to version 3.25.34, but was changed to the `propValues` property from version 3.25.35 onwards.
+						object._zod.disc = item._zod.disc;
+						return object;
+					};
+					if (objectDef.type !== 'object') {
+						return setOriginalPropValues(enableTypeCoercion(item, options));
+					}
 					const object = new item._zod.constr({
 						...objectDef,
 						shape: Object.fromEntries(
@@ -284,13 +305,7 @@ export function enableTypeCoercion<Schema extends $ZodType>(
 						),
 					}) as $ZodType<unknown, {}>;
 
-					// The discriminate key is obtained from the defined Object.
-					// If you regenerate the Object schema, the `propValues` property disappears. Therefore, set the one obtained from the original Object.
-					// https://github.com/colinhacks/zod/blob/22ab436bc214d86d740e78f33ae6834d28ddc152/packages/zod/src/v4/core/schemas.ts#L1949-L1963
-					object._zod.propValues = item._zod.propValues;
-					// @ts-expect-error: The `disc` property was used up to version 3.25.34, but was changed to the `propValues` property from version 3.25.35 onwards.
-					object._zod.disc = item._zod.disc;
-					return object;
+					return setOriginalPropValues(object);
 				}),
 			}) as $ZodType<unknown, {}>,
 		);
--- packages/conform-zod/v4/tests/coercion/schema/discriminatedUnion.test.ts
+++ packages/conform-zod/v4/tests/coercion/schema/discriminatedUnion.test.ts
@@ -7,6 +7,7 @@ import {
 	literal,
 	number,
 	boolean,
+	extend,
 } from 'zod/v4-mini';
 import { getResult } from '../../../../tests/helpers/zod';
 
@@ -123,6 +124,114 @@ describe('coercion', () => {
 				success: false,
 				error: {
 					'nest.type': ['Invalid input'],
+				},
+			});
+		});
+
+		test('should pass composed discriminatedUnion', () => {
+			const base = z.object({
+				status: z.literal('failed'),
+				message: z.string(),
+			});
+			const schema = z.discriminatedUnion('status', [
+				z.object({ status: z.literal('success'), data: z.string() }),
+				z.discriminatedUnion('code', [
+					base.extend({ code: z.literal(400) }),
+					base.extend({ code: z.literal(401) }),
+					base.extend({ code: z.literal(500) }),
+				]),
+			]);
+
+			const baseWithMini = object({
+				status: literal('failed'),
+				message: z.string(),
+			});
+			const schemaWithMini = discriminatedUnion('status', [
+				object({ status: literal('success'), data: z.string() }),
+				discriminatedUnion('code', [
+					extend(baseWithMini, { code: literal(400) }),
+					extend(baseWithMini, { code: literal(401) }),
+					extend(baseWithMini, { code: literal(500) }),
+				]),
+			]);
+
+			expect(
+				getResult(
+					coerceFormValue(schema).safeParse({
+						status: 'success',
+						data: 'test',
+					}),
+				),
+			).toEqual({
+				success: true,
+				data: {
+					status: 'success',
+					data: 'test',
+				},
+			});
+			expect(
+				getResult(
+					coerceFormValue(schema).safeParse({
+						status: 'failed',
+						message: 'error',
+						code: 400,
+					}),
+				),
+			).toEqual({
+				success: true,
+				data: {
+					status: 'failed',
+					message: 'error',
+					code: 400,
+				},
+			});
+			expect(
+				getResult(coerceFormValue(schema).safeParse({ status: 'none' })),
+			).toEqual({
+				success: false,
+				error: {
+					status: ['Invalid input'],
+				},
+			});
+
+			expect(
+				getResult(
+					coerceFormValue(schemaWithMini).safeParse({
+						status: 'success',
+						data: 'test',
+					}),
+				),
+			).toEqual({
+				success: true,
+				data: {
+					status: 'success',
+					data: 'test',
+				},
+			});
+			expect(
+				getResult(
+					coerceFormValue(schemaWithMini).safeParse({
+						status: 'failed',
+						message: 'error',
+						code: 400,
+					}),
+				),
+			).toEqual({
+				success: true,
+				data: {
+					status: 'failed',
+					message: 'error',
+					code: 400,
+				},
+			});
+			expect(
+				getResult(
+					coerceFormValue(schemaWithMini).safeParse({ status: 'none' }),
+				),
+			).toEqual({
+				success: false,
+				error: {
+					status: ['Invalid input'],
 				},
 			});
 		});
