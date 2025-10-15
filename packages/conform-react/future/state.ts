c2df9c02a6de8d2eafe7360fc6cd9d5e1d5acecf
Edmund Hung <edmund@cloudflare.com>
2025-10-15 23:50:32 +0100

    fixup! fix(conform-react/future): handle React DevTools inspection of field metadata

--- packages/conform-react/future/state.ts
+++ packages/conform-react/future/state.ts
@@ -465,13 +465,15 @@ export function getField<FieldShape, ErrorShape = string>(
 		return metadata;
 	}
 
+	let customMetadata: Record<string, unknown> | null = null;
+
 	return new Proxy(metadata, {
 		get(target, prop, receiver) {
 			if (Reflect.has(target, prop)) {
 				return Reflect.get(target, prop, receiver);
 			}
 
-			const customMetadata = customize(metadata);
+			customMetadata ??= customize(metadata);
 
 			if (Reflect.has(customMetadata, prop)) {
 				return Reflect.get(customMetadata, prop, receiver);
@@ -479,7 +481,7 @@ export function getField<FieldShape, ErrorShape = string>(
 
 			// Allow React DevTools to inspect the object
 			// without throwing errors for internal properties
-			if (typeof prop === 'symbol') {
+			if (typeof prop === 'symbol' || prop === '$$typeof') {
 				return undefined;
 			}
 
